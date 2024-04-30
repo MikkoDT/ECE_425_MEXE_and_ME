0587a4b261acd8d1c309862e71c06aac6bf9d234
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-04-30 16:48:51 +0800

    update

---
+++ MATLAB_activities/Step_4205/step.m
@@ -0,0 +1,19 @@
+% Clear
+clear
+clc
+close all
+
+%% Define Transfer Function
+% Mechanical Translational System (Mass, Spring, Damper)
+% G(s) = 1 / s^2 + s + 1
+
+M = 10;
+B = 15;
+k = 20;
+
+G_num = [1];
+G_den = [M B k];
+G = tf(G_num,G_den)
+
+% step response
+step(G,0:0.1:20)
